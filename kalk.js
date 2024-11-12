3872ec02c51b45ad15ac9fffff3750a53743abd2
taepers2007 <mishaninnikita31@gmail.com>
2024-11-12 22:32:32 +0300

    Add files via upload

---
+++ kalk.js
@@ -0,0 +1,29 @@
+    //перменнная в которой хранится выбраное математическое действие 
+    var op;
+    //функция расчета 
+    function func() { 
+    //перемонная для результата
+    var result;
+    // получаем первое и второе число
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+        //смотрим,что было в переменной с действием, и действуем исходя из этого
+        switch (op){
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+        }   
+
+        //отправляем результат на  страницу
+        document.getElementById("result").innerHTML = result;
+    }
+
